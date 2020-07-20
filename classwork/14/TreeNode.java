2d4de470119097499f3c2952300827a0f59e1b59
Ken Chung <ask.mr.chung@gmail.com>
2020-07-20 12:26:16 -0400

    part1 - done

---
+++ classwork/14/TreeNode.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class TreeNode {
+	private int data;
+	private TreeNode left;
+	private TreeNode right;
+	
+	public TreeNode() {
+		this(0);
+	}
+	
+	public TreeNode(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
+	
+	public TreeNode getLeft() {
+		return this.left;
+	}
+	
+	public TreeNode getRight() {
+		return this.right;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+	
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+	
+}//end of class
